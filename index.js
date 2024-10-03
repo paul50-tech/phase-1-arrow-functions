8ef6f7e49e72fcf1fecdc09fb61663c76887a7aa
Paul Jubilee Owuoth <pauljubilee50@gmail.com>
2024-10-03 06:14:51 +0300

    completes lab

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+ 
+  const square = (x) => {
+    return x * x;
+  };
+  
+
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+ 
+  console.log(divide(2000, 100));
+  console.log(square(4));
+  console.log(add(5, 3));
+  
